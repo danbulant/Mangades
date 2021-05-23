5c50b5447e4ef0ac11107b4397638853b40b5691
Daniel Bulant <danbulant@gmail.com>
2021-05-23 17:44:50 +0200

    show errors in page

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,2 +1,28 @@
+<script>
+    import { logs } from "../util/logs";
+</script>
+
 <!-- routify:options preload="proximity" -->
 <slot />
+
+{#if $logs.length}
+    <div class="flow">
+        {#each $logs as item}
+            <div class="item {item.type}">
+                {item.text}
+            </div>
+        {/each}
+    </div>
+{/if}
+
+<style>
+    .flow {
+        position: fixed;
+        bottom: 0;
+        right: 0;
+        background: white;
+        border-radius: 5px 0 0 0;
+        padding: 5px;
+        box-shadow: 0 0 2px 0 black;
+    }
+</style>
