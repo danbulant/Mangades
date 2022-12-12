984e3bf7ec1664266d0a6b1461237c1988ca8505
Daniel Bulant <danbulant@gmail.com>
2022-12-12 20:09:22 +0100

    add browser tracing

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,6 @@
 import { Toucan } from "toucan-js";
 import * as Sentry from '@sentry/browser';
+import { BrowserTracing } from "@sentry/tracing";
 
 /** @type {import('./$types').PageLoad} */
 export function load({ url, event }) {
@@ -39,7 +40,11 @@ export function load({ url, event }) {
                 // @ts-ignore
                 environment: import.meta.env.VITE_SENTRY_ENVIRONMENT,
                 // @ts-ignore
-                release: import.meta.env.VITE_SENTRY_RELEASE
+                release: import.meta.env.VITE_SENTRY_RELEASE,
+                integrations: [
+                    new BrowserTracing(),
+                ],
+                tracesSampleRate: 1
             });
 
             sentry.isInitalized = true;
