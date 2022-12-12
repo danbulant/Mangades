6c7df61f5665ace824f794dd4c4b8aeffd9aa1c1
Daniel Bulant <danbulant@gmail.com>
2022-12-12 16:10:24 +0100

    sentry handle errors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,37 @@
-import * as Sentry from "@sentry/node";
+import { Toucan } from 'toucan-js';
+
 /** @type {import('@sveltejs/kit').HandleClientError} */
 export function handleError({ error, event }) {
-    // @ts-ignore
-    Sentry.captureException(error, { event });
-   
+        // @ts-ignore
+    if(import.meta.env.VITE_SENTRY_DSN !== undefined) {
+        const sentry = new Toucan({
+            // @ts-ignore
+            dsn: import.meta.env.VITE_SENTRY_DSN,
+
+            // @ts-ignore
+            environment: import.meta.env.VITE_SENTRY_ENVIRONMENT,
+            // @ts-ignore
+            release: import.meta.env.VITE_SENTRY_RELEASE,
+            request: event.request,
+            requestDataOptions: {
+                allowedHeaders: [
+                    'user-agent',
+                    'cf-challenge',
+                    'accept-encoding',
+                    'accept-language',
+                    'cf-ray',
+                    'content-length',
+                    'content-type',
+                    'x-real-ip',
+                    'host',
+                ],
+                allowedSearchParams: /(.*)/
+            }
+        });
+
+        sentry.captureException(error);
+    }
+
     return {
         ...error,
         code: error?.code ?? 'UNKNOWN'
