2a65e607f2eadf24a850479a611656a7a25ecb43
Daniel Bulant <danbulant@gmail.com>
2023-08-22 12:11:38 +0200

    add debug mode

---
+++ src/routes/cors-anywhere/dbg/+server.ts
@@ -0,0 +1,17 @@
+import { json } from '@sveltejs/kit';
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ url }) {
+    console.log("[CORS-DBG] " + url.searchParams.get('url'));
+    const ret = await fetch(url.searchParams.get('url'));
+    let text = await ret.text();
+    let data = text;
+    try {
+        data = JSON.parse(text);
+    } catch (e) {}
+
+    return json({
+        headers: Object.fromEntries(ret.headers.entries()),
+        body: data
+    });
+}
